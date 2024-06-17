8ff036855a880f9c8ef0803baa86c326f5a9d621
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-06-17 10:15:07 +0000

    format

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,18 @@
-import type { Component, JSX } from "solid-js";
+import type { Component, JSX } from 'solid-js'
 
 export const Button: Component<{
-  id: string;
-  name: string;
-  onClick: JSX.CustomEventHandlersCamelCase<HTMLButtonElement>["onClick"];
+  id: string
+  name: string
+  onClick: JSX.CustomEventHandlersCamelCase<HTMLButtonElement>['onClick']
 }> = (props) => {
   return (
     <button
-      type="button"
-      class="ring-offset-background focus-visible:ring-ring inline-flex h-10 w-full items-center justify-center rounded-md bg-neutral-dark-5 px-4 py-2 text-sm font-medium text-primary-1 transition-colors hover:bg-neutral-dark-3/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+      type='button'
+      class='ring-offset-background focus-visible:ring-ring inline-flex h-10 w-full items-center justify-center rounded-md bg-neutral-dark-5 px-4 py-2 text-sm font-medium text-primary-1 transition-colors hover:bg-neutral-dark-3/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50'
       id={props.id}
       onClick={props.onClick}
     >
       {props.name}
     </button>
-  );
-};
+  )
+}
--- src/routes/api/[slug].ts
+++ src/routes/api/[slug].ts
@@ -1,28 +1,28 @@
-import type { APIEvent } from "@solidjs/start/server";
-import { eq } from "drizzle-orm";
-import { paste } from "../../core/schema";
-import { db } from "../../core/db";
-import { json } from "@solidjs/router";
+import type { APIEvent } from '@solidjs/start/server'
+import { eq } from 'drizzle-orm'
+import { paste } from '../../core/schema'
+import { db } from '../../core/db'
+import { json } from '@solidjs/router'
 
 export async function GET(event: APIEvent) {
-  const slug = event.params.slug;
+  const slug = event.params.slug
   const [result] = await db
     .select({
       slug: paste.slug,
       content: paste.content,
       createdAt: paste.createdAt,
       updatedAt: paste.updatedAt,
-      expiresAt: paste.expiresAt,
+      expiresAt: paste.expiresAt
     })
     .from(paste)
     .where(eq(paste.slug, slug))
     .limit(1)
-    .execute();
+    .execute()
 
   if (!result)
-    return new Response("Could not find that paste.", {
-      status: 404,
-    });
+    return new Response('Could not find that paste.', {
+      status: 404
+    })
 
-  return json(result);
+  return json(result)
 }
